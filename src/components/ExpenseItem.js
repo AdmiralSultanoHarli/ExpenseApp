3f0451d7957cd2228f63b7cd1463c66920778043
AdmiralSultano <giordano2010@gmail.com>
2021-12-21 14:34:06 +0700

    Moving the data using props and add ExpenseDate

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
@@ -5,17 +6,11 @@ const expenseDate = new Date(2021, 12, 21);
 const expenseTitle = 'Car Insurance';
 const expenseAmount = 294.67;
 
-const day = props.date.toLocaleString('id-ID', {day: '2-digit'});
-const month = props.date.toLocaleString('id-ID', {month: 'long'});
-const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate 
+        date={props.date}
+      />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
