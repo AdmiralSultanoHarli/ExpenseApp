2b758e1adf27bdcfc493320675741b6b42a53476
AdmiralSultano <giordano2010@gmail.com>
2021-12-21 13:53:34 +0700

    Using props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,9 +5,17 @@ const expenseDate = new Date(2021, 12, 21);
 const expenseTitle = 'Car Insurance';
 const expenseAmount = 294.67;
 
+const day = props.date.toLocaleString('id-ID', {day: '2-digit'});
+const month = props.date.toLocaleString('id-ID', {month: 'long'});
+const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
