a3b593544f2b97595985bfa3adc9fddec9c89ecb
AdmiralSultano <giordano2010@gmail.com>
2021-12-22 11:17:07 +0700

    Organizing component files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Expenses from "./components/Expenses";
+import React from "react";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -28,12 +29,21 @@ function App() {
     },
   ];
 
+  // Method 1 
   return (
     <div>
       <h2>Let's get started!</h2>
       <Expenses expenses={expenses}/>
     </div>
   );
+
+  // Method 2
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started!"),
+  //   React.createElement(Expenses, {expenses: expenses})
+  // );
 }
 
 export default App;
