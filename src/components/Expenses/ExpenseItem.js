9356ddfbef6c7760f3403fc91131eafabce04894
AdmiralSultano <giordano2010@gmail.com>
2021-12-22 15:27:04 +0700

    add onclick function and newexpense

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
-const expenseDate = new Date(2021, 12, 21);
-const expenseTitle = 'Car Insurance';
-const expenseAmount = 294.67;
+  const expenseDate = new Date(2021, 12, 21);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+
+  const clickHandler = () => {
+    setTitle('Updated');
+    console.log(title);
+  }
 
   return (
     <Card className="expense-item">
@@ -14,9 +22,10 @@ const expenseAmount = 294.67;
         date={props.date}
       />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={() => {clickHandler()}}>Change Title</button>
     </Card>
   );
 }
