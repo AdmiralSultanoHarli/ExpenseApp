035e0621aef9a937adefcc911a7baa6576c9b826
AdmiralSultano <giordano2010@gmail.com>
2021-12-28 11:42:22 +0700

    Assignment 4 : conditional content

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -80,7 +80,8 @@ const ExpenseForm = (props) => {
             </div>
         </div>
         <div className="new-expense__actions">
-            <button>Add Expense</button>
+            <button type="button" onClick={props.onCancel}>Cancel</button>
+            <button type="submit">Add Expense</button>
         </div>
     </form>
     );
