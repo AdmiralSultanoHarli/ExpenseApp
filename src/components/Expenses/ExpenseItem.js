99081ed3c0c4f178332284d5c4a5d12c2ad8a69c
AdmiralSultano <giordano2010@gmail.com>
2021-12-28 11:23:47 +0700

    Adding conditional return statements

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,11 @@
-import React, {useState} from "react";
-import ExpenseDate from './ExpenseDate';
+import React, { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   const expenseDate = new Date(2021, 12, 21);
-  const expenseTitle = 'Car Insurance';
+  const expenseTitle = "Car Insurance";
   const expenseAmount = 294.67;
 
   // const [title, setTitle] = useState(props.title);
@@ -16,17 +16,17 @@ const ExpenseItem = (props) => {
   // }
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate 
-        date={props.date}
-      />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      {/* <button onClick={() => {clickHandler()}}>Change Title</button> */}
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+        {/* <button onClick={() => {clickHandler()}}>Change Title</button> */}
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
